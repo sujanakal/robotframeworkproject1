bb9274640d3b07e36a8e9a7065b55493d46774ae
Dineshkumar, Sujana <Sujana.Dineshkumar@emc.com>
2020-06-12 17:11:43 +0530

    Added code 'execution.py' to handle different execution ways

---
+++ batchrun.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\diness\Documents\Automation\Robot_Framework\RobotProject1
+call python execution.py suite
--- execution.py
+++ execution.py
@@ -5,13 +5,34 @@
 import os
 import sys
 import datetime
+import subprocess
 
 current_datetime = datetime.datetime.now().strftime("%d%m%y_%H%M%S.%f")
 arg = sys.argv[1]
 cmd = ''
 
-# Command to execute entire suite; Argument = 'suite'
-if arg == 'suite':
-    cmd = "robot -d results/test_suite_" + current_datetime + " tests/test_suite.robot"
+if arg == 'single':
+    # This is to execute the single robot file
+    file_name = sys.argv[2]
+    cmd = "robot -d results/test/" + current_datetime + " /tests/" + file_name
+
+elif arg == 'suite':
+    suite_name = sys.argv[2]
+    cmd = "robot -d results/suite"+ suite_name +"/" + current_datetime + " tests/" + suite_name
+
+elif arg == 'all_suites':
+    # This is to execute the multiple suites (entire tests folder)
+    cmd = "robot -d results/full_reg/" + current_datetime + " tests"
+
+elif arg == 'batch':
+    #This is to execute the batch file
+    subprocess.call([r'batchrun.bat'])
+    cmd = 'echo Batch execution complete'
+
+elif arg == 'tag':
+    # This is to execute the files based on tags
+    tag = sys.argv[2]
+    file_name = sys.argv[3]
+    cmd = "robot -d results/" + tag + "/" + current_datetime + " /tests/" + file_name
 
 os.system(cmd)
